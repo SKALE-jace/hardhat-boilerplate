b6feb9249c8f2b4ed07e15a2a4e18a26b50fce42
SKALE-jace <103141574+SKALE-jace@users.noreply.github.com>
2022-07-22 16:47:52 +0100

    Create HelloSkale.sol
    added HelloWorld solidity for Skale

---
+++ contracts/HelloSkale.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract HelloSkale {
+    function sayHelloSkale() public pure returns (string memory) {
+        return "Hello Skale!";
+    }
+}
